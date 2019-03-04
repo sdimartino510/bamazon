e869a743e66d05ec3032382348b3c9b2e86ecbd5
Sergio Di Martino <sergio@webdevserg.io>
2019-03-03 18:53:10 -0800

    Adds database connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    connection.query("SELECT * from products", function(err, result) {
+
+    if (err) throw err;
+    console.log("Result:\n", result);
+
+});
+
+connection.end();
+});
